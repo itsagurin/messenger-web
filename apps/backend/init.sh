3078de9c172b4895f20fd0e15c9d7c39dd2ab206
Alex Huryn <algurin77@gmail.com>
2025-02-07 22:45:10 +0100

    docker bug fix

--- apps/backend/init.sh
+++ apps/backend/init.sh
@@ -1,20 +1,9 @@
 #!/bin/sh
 
-echo "Waiting for PostgreSQL to start..."
-until pg_isready -h db -U ${POSTGRES_USER} -d ${POSTGRES_DB}
-do
-  echo "Waiting for database connection..."
-  sleep 2
-done
-echo "PostgreSQL started"
-
 echo "Running migrations..."
 npx prisma migrate deploy
-echo "Migrations completed"
-
 echo "Generating Prisma client..."
 npx prisma generate
-echo "Prisma client generated"
 
 echo "Starting application..."
 npm run start
