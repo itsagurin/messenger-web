33bc074715b487bf4c8feaa18ea01bc25c246460
Alex Huryn <algurin77@gmail.com>
2025-03-10 14:54:24 +0100

    fix validators

--- apps/backend/src/message/dto/create-message.dto.ts
+++ apps/backend/src/message/dto/create-message.dto.ts
@@ -1,5 +1,15 @@
+import { IsNotEmpty, IsNumber, IsString, Min, IsPositive } from 'class-validator';
+
 export class CreateMessageDto {
+  @IsNumber()
+  @IsPositive()
   senderId: number;
+
+  @IsNumber()
+  @IsPositive()
   receiverId: number;
+
+  @IsNotEmpty()
+  @IsString()
   text: string;
 }
--- apps/backend/src/payment/dto/subscription.dto.ts
+++ apps/backend/src/payment/dto/subscription.dto.ts
@@ -1,18 +1,15 @@
 import { PlanType } from '@prisma/client';
-import { IsNumber, IsNotEmpty, IsEnum } from 'class-validator';
+import { IsNumber, IsEnum } from 'class-validator';
 
 export class CreateSubscriptionDto {
   @IsNumber()
-  @IsNotEmpty()
   userId: number;
 
   @IsEnum(PlanType)
-  @IsNotEmpty()
   planType: PlanType;
 }
 
 export class UpdateSubscriptionDto {
   @IsEnum(PlanType)
-  @IsNotEmpty()
   planType: PlanType;
 }
